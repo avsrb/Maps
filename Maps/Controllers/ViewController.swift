401211dd411901f79c3d71e1c7f0b7a3aba18874
Artem <Serebryakov>
2022-08-17 10:33:58 +0300

    Init

--- Maps/Controllers/ViewController.swift
+++ Maps/Controllers/ViewController.swift
@@ -11,9 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .red
         // Do any additional setup after loading the view.
     }
-
 
 }
 
