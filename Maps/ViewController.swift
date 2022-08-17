4a219061115db18451b68dc8666dfc8790ad1a66
Artem <Serebryakov>
2022-08-17 09:55:47 +0300

    Initial Commit

---
+++ Maps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Maps
+//
+//  Created by Artem Serebriakov on 17.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
