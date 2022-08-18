9a09a0537edbd34f20484a413bf8dd8bfe1f29b4
Artem <Serebryakov>
2022-08-18 15:11:18 +0300

    color

--- Maps/Controllers/MapViewController.swift
+++ Maps/Controllers/MapViewController.swift
@@ -10,9 +10,9 @@ import MapKit
 
 class MapViewController: UIViewController {
 
-    let mapView = MKMapView()
-    let locationManager = CLLocationManager()
-    let segmentedControll : UISegmentedControl = {
+    private lazy var mapView = MKMapView()
+    private lazy var locationManager = CLLocationManager()
+    private lazy var segmentedControll : UISegmentedControl = {
         let segmentedControll = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
         segmentedControll.selectedSegmentIndex = 0
         segmentedControll.selectedSegmentTintColor = .systemBlue
@@ -23,18 +23,40 @@ class MapViewController: UIViewController {
         segmentedControll.addTarget(self, action: #selector(changeMapViewType(_:)), for: .valueChanged)
         return segmentedControll
     }()
+    private lazy var locationButton : UIButton = {
+        let button = UIButton()
+        let configuration = UIImage.SymbolConfiguration(pointSize: segmentedControll.frame.height - 5)
+        let image = UIImage(systemName: "location.fill", withConfiguration: configuration)
+        button.setImage(image, for: UIControl.State.normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(curentLocation), for: .touchUpInside)
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 //        view.backgroundColor = .systemBackground
         mapView.addSubview(segmentedControll)
+        mapView.addSubview(locationButton)
+
         view = mapView
+        
+        mapView.delegate = self
+
         mapView.showsUserLocation = true
-        let initialLocation = CLLocation(latitude: 55.796289, longitude: 49.108795)
-        mapView.centerLocation(initialLocation)
+        let initialLocation = CLLocation(latitude: Place.favotite.first!.coordinate.latitude, longitude: Place.favotite.first!.coordinate.longitude)
+        mapView.centerToLocation(initialLocation)
         
         segmentedControll.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
         segmentedControll.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        locationButton.leftAnchor.constraint(equalTo: segmentedControll.rightAnchor, constant: 10).isActive = true
+        locationButton.topAnchor.constraint(equalTo: segmentedControll.topAnchor).isActive = true
+        
+        for place in Place.favotite {
+            mapView.addAnnotation(place)
+        }
+
     }
     
     @objc func changeMapViewType(_ sender: UISegmentedControl) {
@@ -50,28 +72,27 @@ class MapViewController: UIViewController {
         }
     }
     
-}
-
-extension MKMapView {
-    func centerLocation(_ location: CLLocation, regionRadius: CLLocationDistance = 1000) {
-        let coordinateRegion = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
-        setRegion(coordinateRegion, animated: true)
+    @objc func curentLocation () {
+        print("curentLocation")
     }
-}
-
-func getCoordinateFrom(city: String, completion: @escaping(_ coordinate: CLLocationCoordinate2D?, _ error: Error?) -> () ) {
-    CLGeocoder().geocodeAddressString(city) { (placemark, error) in
-        completion(placemark?.first?.location?.coordinate, error)
-    }
+    
 }
 
 extension MapViewController: PresenterToMapViewProtocol {
     func goToplace(place: Place) {
         self.tabBarController?.selectedIndex = 0
-        let center = place.coordinate
-        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-        self.mapView.setRegion(region, animated: true)
-        
+        let location = CLLocation(latitude: place.coordinate.latitude, longitude: place.coordinate.longitude)
+        self.mapView.centerToLocation(location)
+    }
+}
+
+private extension MKMapView {
+    func centerToLocation(_ location: CLLocation, regionRadius: CLLocationDistance = 1000) {
+        let coordinateRegion = MKCoordinateRegion(
+            center: location.coordinate,
+            latitudinalMeters: regionRadius,
+            longitudinalMeters: regionRadius)
+        setRegion(coordinateRegion, animated: true)
     }
 }
 
@@ -81,26 +102,23 @@ extension MapViewController: MKMapViewDelegate {
             return nil
         }
         let identifier = "place"
-        let view: MKMarkerAnnotationView
-        if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: "place") as? MKMarkerAnnotationView {
+        var view: MKMarkerAnnotationView
+        
+        if let dequeuedView = mapView.dequeueReusableAnnotationView( withIdentifier: identifier) as? MKMarkerAnnotationView {
             dequeuedView.annotation = annotation
             view = dequeuedView
         } else {
             view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
             view.canShowCallout = true
+            for place in Place.favotite {
+                if place.title == annotation.title {
+                    view.markerTintColor = place.color
+                }
+            }
             view.calloutOffset = CGPoint(x: -5, y: 5)
             view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
         }
         return view
     }
     
-    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
-        guard let place = view.annotation as? Place else {
-            return
-        }
-        
-        let launchOption = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
-        
-        
-    }
 }
--- Maps/Model.swift
+++ Maps/Model.swift
@@ -9,21 +9,52 @@ import MapKit
 import UIKit
 
 class Place: NSObject, MKAnnotation {
-    var title: String?
-    var coordinate: CLLocationCoordinate2D
-    var info: String
-    var color: UIColor
+    let title: String?
+    let locationName: String?
+    let discipline: String?
+    let coordinate: CLLocationCoordinate2D
+    let color: UIColor
     
-    init(title: String, coordinate: CLLocationCoordinate2D, info: String, color: UIColor = .red) {
-        self.title = title
-        self.coordinate = coordinate
-        self.info = info
-        self.color = color
+    init(title: String?, locationName: String?, discipline: String?, coordinate: CLLocationCoordinate2D, color: UIColor = .red) {
+      self.title = title
+      self.locationName = locationName
+      self.discipline = discipline
+      self.coordinate = coordinate
+      self.color = color
+      super.init()
     }
 
+    var subtitle: String? {
+      return locationName
+    }
+    
+    
     static let favotite = [
-        Place(title: "Kaзань", coordinate:CLLocationCoordinate2D(latitude: 55.796289, longitude: 49.108795), info: "21 school"),
-        Place(title: "Wolfsburg", coordinate:CLLocationCoordinate2D(latitude: 52.427911, longitude: 10.785847), info: "42 Wolfsburg"),
-        Place(title: "USA", coordinate:CLLocationCoordinate2D(latitude: 38.899513,  longitude: -77.036527), info: "United State"),
+        Place(title: "Kaзань",
+              locationName: "School21",
+              discipline: "👨‍💻",
+              coordinate: CLLocationCoordinate2D(latitude: 55.796289, longitude: 49.108795),
+              color: .green),
+        Place(title: "42",
+              locationName: "Wolfsburg",
+              discipline: "🛩",
+              coordinate: CLLocationCoordinate2D(latitude: 52.428048, longitude: 10.790464),
+              color: .black),
+        Place(title: "Дом Ксюши",
+              locationName: "Тольятти",
+              discipline: "😅",
+              coordinate: CLLocationCoordinate2D(latitude: 53.484884, longitude: 49.464900),
+              color: .systemPink),
+        Place(title: "Челябинск",
+              locationName: "Тольятти",
+              discipline: "🚂",
+              coordinate: CLLocationCoordinate2D(latitude: 55.159902, longitude: 61.402554),
+              color: .orange),
+        Place(title: "Энрики",
+              locationName: "Тбилиси",
+              discipline: "🍇",
+              coordinate: CLLocationCoordinate2D(latitude: 41.706844, longitude: 44.806439),
+              color: .opaqueSeparator),
+        
     ]
 }
